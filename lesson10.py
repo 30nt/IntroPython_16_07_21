b1bbf0d2580fc6170e88dfc57f82842eab9f2f16
30nt <zontovv@outlook.com>
2021-08-13 23:13:13 +0300

    lesson10

---
+++ lesson10.py
@@ -0,0 +1,67 @@
+# модуль os
+# работа с файлами
+
+import os
+
+
+def add_suffix_to_filename(filename, suffix):
+    filename, ext = filename.rsplit(".", 1)
+    filename = f"{filename}_{suffix}.{ext}"
+    return filename
+
+
+path = "HW"
+filename = "lesson9.txt"
+path_filename = os.path.join(path, filename)
+
+# txt_file = open(path_filename, "r")
+# data = txt_file.read()
+# txt_file.close()
+
+# with open(path_filename, "r") as txt_file:
+#     data = []
+#     for line in txt_file.readlines():
+#         data.append(line.strip())
+#
+# data.append("TEST")
+#
+# new_filename = add_suffix_to_filename(filename, "copy")
+# with open(os.path.join(path, new_filename), "w") as txt_file:
+#     # txt_file.write("\n".join(data))
+#     data = [f"{line}\n" for line in data]
+#     txt_file.writelines(data)
+
+# print(type(data))
+# print(data)
+
+
+# list_dir = os.listdir(path)
+# print(list_dir)
+# files = []
+# for filename in list_dir:
+#     path_filename = os.path.join(path, filename)
+#     check_file = os.path.isfile(path_filename)   # f"{path}/{filename}" - не очень хорошо
+#     if check_file:
+#         files.append(filename)
+# print(files)
+#
+# current_dir = os.getcwd()
+# print(current_dir)
+
+
+# EASY
+# my_str = "abcdefg"
+# result = []
+#
+# my_str = f"{my_str}_" if len(my_str) % 2 else my_str
+# for index in range(0, len(my_str), 2):
+#     result.append(my_str[index: index + 2])
+################
+# for index, value in enumerate(my_str):
+#     if index % 2 == 0:
+#         couple = my_str[index:index+2]
+#         if len(couple)>1:
+#             result.append(couple)
+#         else:
+#             result.append(value+"_")
+# print("EASY", result)
--- lesson10_pr.py
+++ lesson10_pr.py
@@ -4,3 +4,38 @@
 # в которых будет записана строка алфавита, но с заменой буквы из названия файла на прописную.
 # Пример: для b.txt строка будет aBcde...
 # 3. Сделать щелчек Таноса - удалить случайным образом половину всех файлов в этой папке.
+import os
+from string import ascii_lowercase as alphabet
+import random
+
+def create_dir(dir_name):
+    os.makedirs(dir_name, exist_ok=True)
+    # try:
+    #     os.mkdir(dir_name)
+    # except FileExistsError:
+    #     pass
+
+
+def create_file(dirname, symbol):
+    filename = f"{dirname}/{symbol}.txt"
+    with open(filename, "w") as file:
+        file.write(alphabet.replace(symbol, symbol.upper()))
+
+
+def create_files(dirname):
+    for symbol in alphabet:
+        create_file(dirname, symbol)
+
+def do_tanos_click(dirname):
+    files = os.listdir(dirname)
+    random.shuffle(files)
+    # files = list(set(files))
+    for filename in files[:len(files) // 2]:
+        os.remove(os.path.join(dirname, filename))
+
+
+
+dirname = "alphabet"
+create_dir(dirname)
+create_files(dirname)
+do_tanos_click(dirname)
